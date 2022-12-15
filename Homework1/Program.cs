38032a9590a95f173654a18fda97a390b614483b
Marina <mariness13051986@mail.ru>
2022-12-15 17:49:45 +0700

    init

---
+++ Homework1/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+{
+    Console.Write("This numbers is equal");
+}
+else 
+if (num1 > num2)
+{
+    Console.Write($"Number {num1} is bigger than number {num2}.");
+}
+else
+{
+    Console.Write($"Number {num2} is bigger than number {num1}.");
+}
+*/
+
+/*
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input the First number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the Second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the Third number: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+Console.Write("Maximum number is: " + max);
+*/
+
+/*
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.Write($"The number {num} is even.");
+}
+else
+{
+    Console.Write($"The number {num} is NOT even.");
+}
+*/
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+/*
+Console.Write("Input number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while(current < N)
+{
+    current++;
+    Console.Write(current + " ");
+    current++;
+}
+Console.WriteLine();
+*/
+
+
---
+++ Homework2/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+int MiddleNum(int num)
+{
+    int mid = num % 100 /10;
+    return mid;
+}
+Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int middle = MiddleNum(num);
+Console.Write("Вторая цифра трёхзначного числа - " + middle);
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+int ThirdNum(int num)
+{
+    if(num < 100)
+    {
+        return -1;
+    }
+    if(num > 999)
+    {
+        while(num > 999)
+        {
+            num = num / 10;
+        }
+    }
+    
+    int thirdNum = num % 10;
+    return thirdNum;
+}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = ThirdNum(num);
+if(result < 0)
+{
+    Console.WriteLine($"В числе {num} нет третьей цифры.");
+}
+else
+{
+    Console.WriteLine($"Третья цифра числа {num} - {result}.");
+}
+*/
+
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+Console.WriteLine("Введите день недели: ");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+bool CheckDay(int num)
+{
+
+    if (num == 6 || num == 7) 
+    {
+        Console.WriteLine("Ответ: Выходной день.");
+        return true;
+    }
+    else if  (num < 1 || num > 7)
+    {
+        Console.WriteLine("Ошибка: В неделе всего 7 дней!");
+        return false;
+    }
+    else
+    {
+        Console.WriteLine("Ответ: Будний день.");
+        return false;
+    }
+}
+
+
+bool day = CheckDay(dayOfWeek);
+Console.WriteLine(day);
+*/
