32e67653de4e5b52b9bca6f660fea313e5306815
Marina <mariness13051986@mail.ru>
2022-12-29 18:29:23 +0700

    task 47 correct

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
+/*
 Console.WriteLine("введите количество строк: ");
 int linesVol = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите количество столбцов: ");
@@ -30,4 +30,5 @@ void PrintArray(double[,] array)
         Console.WriteLine("");
     }
 }
+*/
 
