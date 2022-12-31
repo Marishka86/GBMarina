a95946744537fee8d7885e1a2beea70b013b7251
Marina <mariness13051986@mail.ru>
2022-12-31 12:23:22 +0700

    task 68

--- Homework9last/Program.cs
+++ Homework9last/Program.cs
@@ -38,3 +38,31 @@
      int res = SumNumbers(m,n);
      Console.WriteLine("Sum: " + (res));
      */
+     //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.  Даны два неотрицательных числа m и n.
+/*
+    int Ackermann(int m, int n)
+    {
+         
+            if (m == 0)
+            {
+               return n + 1;
+            }
+            if (m > 0 && n == 0)
+            {
+                return Ackermann(m - 1, 1);
+            }
+            else
+            {
+               return Ackermann(m - 1, Ackermann(m, n - 1));
+            }
+              
+    }
+    
+    Console.Write("Enter number m: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Enter number n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int res = Ackermann(m,n);
+    Console.WriteLine($"A(m,n) = {res}");
+    */
+    
